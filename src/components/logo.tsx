c242c9c1fc5b03e4919a2e2b6f5481cd83e23cb7
victorbraga <victor.medeiros@contraprova.com.br>
2023-02-02 21:57:17 -0300

    Atualização de SRC da Logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,5 @@
 export function Logo({ className }: { className?: string }) {
   return (
-    <img className="object-scale-down h-40 w-40" src="logo.png" alt="" />
+    <img className="object-scale-down h-40 w-40" src="https://victorbraga.com.br/img/logo-agile.png" alt="" />
   )
 }
